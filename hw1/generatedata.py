26d57785b1a16c4ca71d64f10c1638d50f2bdb57
jbarnett8 <jbarnett8@gatech.edu>
2019-10-04 16:19:49 -0700

    improved data generation

--- hw1/generatedata.py
+++ hw1/generatedata.py
@@ -34,6 +34,14 @@ if any([not s.isdigit() for s in sys.argv[1:3]]):
 #     print_help_text()
 #     sys.exit()
 
+
+# Defining constants given in the assignment document
+PERCENT_RAND_REF_LENGTH = 0.75
+SINGEL_MATCH_CUTOFF = 0.75
+DOUBLE_MATCH_CUTOFF = 0.85
+
+
+# Setting common variables from command line
 num_reads = int(sys.argv[2])
 ref_length = int(sys.argv[1])
 read_length = int(sys.argv[3])
@@ -41,7 +49,7 @@ read_length = int(sys.argv[3])
 # This a check to make sure we have a sane selection for the read length.
 # In order to have doubly aligned readas, the read length has to stay 
 # below a certain threshold
-if (ref_length - math.ceil(0.75*ref_length)) <= read_length:
+if (ref_length - math.ceil(PERCENT_RAND_REF_LENGTH*ref_length)) <= read_length:
     print('The read length is too large. Make it smaller.')
     print_help_text()
     sys.exit()
@@ -49,10 +57,10 @@ if (ref_length - math.ceil(0.75*ref_length)) <= read_length:
 print('reference length: {}\nnumber reads: {}\nread length: {}'.format(
        ref_length, num_reads, read_length))
 
-# first we'll make a reference library random values up to 75% of
-# the specified length
+# first we'll make a reference library random values 
+# up to PERCENT_RAND_REF_LENGTH of the specified length
 bases = ['A', 'T', 'C', 'G']
-rand_ref_length = math.floor(0.75*ref_length)
+rand_ref_length = math.floor(PERCENT_RAND_REF_LENGTH*ref_length)
 rep_ref_length = ref_length - rand_ref_length
 ref_str =  ''.join(bases[random.randint(0,3)] for i in range(rand_ref_length))
 
@@ -70,13 +78,13 @@ for i in range(num_reads):
     rand_num = random.random()
 
     # This is the case for a single alignment: 75% of the time
-    if (rand_num < 0.75):
+    if (rand_num < SINGEL_MATCH_CUTOFF):
         read_align_count[1] += 1
         select = random.randint(0,math.floor(0.5*ref_length))
         reads.append(ref_str[select:select+read_length])
         continue
     # two alignments: 10% of the time
-    elif (rand_num < 0.85):
+    elif (rand_num < DOUBLE_MATCH_CUTOFF):
         read_align_count[2] += 1
         lower = math.ceil(0.75*ref_length)
         upper = ref_length - read_length
@@ -91,7 +99,7 @@ for i in range(num_reads):
                                range(read_length))
             # If the rand_string doesn't exist in the reference string, then
             # we are free to add it, otherwise we try again
-            if (ref_str.find(rand_str) is -1):
+            if rand_str not in ref_str:
                 reads.append(rand_str)
                 break
 
