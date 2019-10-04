c2ef315976fcb17677f4767ede1ccb4f01a9c4be
Joshua Barnett <joshuab8475@gmail.com>
2019-10-04 00:51:16 -0700

    added first submission of data generation script

---
+++ hw1/generatedata.py
@@ -0,0 +1,87 @@
+import random
+import sys
+import os
+import math
+
+def print_help_text(): 
+    print('nreads\t\t:\tThe number of separate ' +
+           'read strings')
+    print('reference_length:\tThe length of ' + 
+           'the reference database')
+    print('read_length\t:\tThe length of each ' +
+           'of the individual read strings')
+    print('reference_file\t:\tThe name of the ' +
+           'reference file to output.')
+    print('reads_file\t:\tThe name of the ' +
+           'reads file to output.')
+
+# These are simple sanity checks, not exhaustive
+if (len(sys.argv) < 6):
+    print('Missing one or more input arguments.\n')
+    print_help_text()
+    sys.exit()
+
+# This ensures that the first three agruments are positive integers
+if any([not s.isdigit() for s in sys.argv[1:3]]):
+    print('One or more of the inputs is not a positive integer.')
+    print_help_text()
+    sys.exit()
+
+# This may not be a necessary check, but we want to make sure we aren't going
+# to ovewrite some other file by accident since it will destroy anything
+if any([os.path.isfile(os.getcwd() + '/' + s) for s in sys.argv[4:6]]):
+    print('One or more of the file names already exists.')
+    print_help_text()
+    sys.exit()
+
+ref_length = int(sys.argv[2])
+read_length = int(sys.argv[3])
+print('reference length: {}\n read length: {}\n'.format(ref_length,
+                                                        read_length))
+
+# first we'll make a reference library random values up to 75% of
+# the specified length
+bases = ['A', 'T', 'C', 'G']
+rand_ref_length = math.floor(0.75*ref_length)
+rep_ref_length = ref_length - rand_ref_length
+ref_str =  ''.join(bases[random.randint(0,3)] for i in range(rand_ref_length))
+
+# Copy the last bit to fill the last 25%
+ref_str = ref_str + ref_str[-rep_ref_length:]
+
+# Write the reference_file string
+with open(sys.argv[4],'w') as f:
+    f.write(ref_str)
+
+# Next we generate the reads
+reads = []
+num_reads = int(sys.argv[1])
+for i in range(num_reads):
+    rand_num = random.random()
+    # This is the case for a single alignment: 75% of the time
+    if (rand_num < 0.75):
+        select = random.randint(0,math.floor(0.5*ref_length))
+        reads.append(ref_str[select:select+read_length])
+        continue
+    # two alignments: 10% of the time
+    elif (rand_num < 0.85):
+        select = random.randint(math.ceil(0.75*ref_length),
+                                          ref_length - read_length)
+        reads.append(ref_str[select:select+read_length])
+        continue
+    # no alignments: 15% of the time
+    else:
+        while True:
+            rand_str = ''.join(bases[random.randint(0,3)] for i in \
+                               range(read_length))
+            # If the rand_string doesn't exist in the reference string, then
+            # we are free to add it, otherwise we try again
+            if (ref_str.find(rand_str) is -1):
+                reads.append(rand_str)
+                break
+
+# Write the result to the specified file
+with open(sys.argv[5],'w') as f:
+    for s in reads:
+        f.write('{}\n'.format(s))
+
